195d3142ad31c2ba5b0ebba538d4b7fc4cf87662
Yizhu ZONG <agnes.lain@gmail.com>
2021-03-30 22:02:11 +0200

    some update of the model knn / notebook test

--- game_one/model_knn_train.py
+++ game_one/model_knn_train.py
@@ -5,15 +5,23 @@ from game_one.model_knn_preprocess import MyCustomPreprocessorKnn
 
 
 class TrainKnn():
+    ''' This class is mean to train the knn model with the matrix data from
+    preprocessor, and we stock the trained model into a pickle file to load
+    '''
 
     def __init__(self):
         self.preprocessor = self.load_prepoc_knn()
         self.neigh = None
+        self.train_model()
 
     def load_prepoc_knn(self):
         with open('preproc.pickle', 'rb') as preproc:
             preproc = pickle.load(preproc)
             return preproc
+
+    def save_model(self, filename):
+        with open(filename, 'wb') as handle:
+            pickle.dump(self.neigh, handle)
 
     def train_model(self):
         df = self.preprocessor.rating_matrix
@@ -22,6 +30,6 @@ class TrainKnn():
         self.neigh = neigh
         self.save_model('knn_model.pickle')
 
-    def save_model(self, filename):
-        with open(filename, 'wb') as handle:
-            pickle.dump(self.neigh, handle)
+if __name__ == "__main__":
+    knn_train = TrainKnn()
+    print('knn model has been trained, ready for predict')
